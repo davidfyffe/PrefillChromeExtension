ba6bb93959abc58739d48b9d80c3c2a53fecc53a
davidfyffe <davidfyffe@yahoo.co.uk>
2018-04-27 11:33:07 +0100

    Update so change is propagated to the Angular model.

--- prefillTextBoxes.js
+++ prefillTextBoxes.js
@@ -3,10 +3,28 @@
 console.log('Executing inside prefillTextBoxes');
 
 chrome.storage.sync.get('dailyHours', function(data) {
-    document.querySelectorAll('*[id^="Work.m"]').forEach(d => d.value= data.dailyHours);
-    document.querySelectorAll('*[id^="Work.t"]').forEach(d => d.value= data.dailyHours);
-    document.querySelectorAll('*[id^="Work.w"]').forEach(d => d.value= data.dailyHours);
-    document.querySelectorAll('*[id^="Work.f"]').forEach(d => d.value= data.dailyHours);
+
+    // Create a new 'change' event required to trigger the anguar model updates.
+    let event = new Event('change');
+
+    let monday = document.getElementById('Work.monday');
+    monday.value = data.dailyHours;
+    monday.dispatchEvent(event);
+
+    let tuethurs = document.querySelectorAll('*[id^="Work.t"]');
+    tuethurs.forEach(d => {
+        d.value = data.dailyHours;
+        d.dispatchEvent(event);
+    });
+
+    let wednesday = document.getElementById('Work.wednesday');
+    wednesday.value = data.dailyHours;
+    wednesday.dispatchEvent(event);
+
+    let friday = document.getElementById('Work.friday');
+    friday.value = data.dailyHours;
+    friday.dispatchEvent(event);
+
 });
 
 chrome.storage.sync.get('weeklyHours', function(data) {
